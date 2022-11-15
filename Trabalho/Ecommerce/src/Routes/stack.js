fd058e76f444c5f8314d09dad5a09be52b4184a7
GabrielNuevo <gabrielalmeidabarrionuevo@hotmail.com>
2022-11-15 13:33:40 -0300

    Criação do projeto e de algumas pastas.

---
+++ Trabalho/Ecommerce/src/Routes/stack.js
@@ -0,0 +1,16 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from "react";
+
+const { Navigator } = createNativeStackNavigator();
+
+export function StackRoutes() {
+    return (
+        <Navigator
+            screenOptions={{
+                headerShown: false,
+            }}>
+
+                
+        </Navigator>
+    )
+}
