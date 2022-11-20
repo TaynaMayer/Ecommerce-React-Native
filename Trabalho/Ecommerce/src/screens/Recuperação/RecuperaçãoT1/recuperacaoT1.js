9f5941aa9f684337937e6d682a372f306703ea3f
Tayná Mayer <112437100+TaynaMayer@users.noreply.github.com>
2022-11-20 16:10:36 -0300

    acrescentando a navegação para a t2 (#7)

--- Trabalho/Ecommerce/src/screens/Recuperação/RecuperaçãoT1/recuperacaoT1.js
+++ Trabalho/Ecommerce/src/screens/Recuperação/RecuperaçãoT1/recuperacaoT1.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
+import { useNavigation } from "@react-navigation/native";
+import { Container } from '../../../components/Global';
 
 // import { Container } from './styles';
 
 const RecuperaçãoT1 = () => {
+  const navigation = useNavigation();
   return (
+
+    <Container>
     <Text>RecuperaçãoT1</Text>
+
+    <TouchableOpacity onPress={() => 
+            navigation.navigate("RecuperaçãoT2")
+        }>
+            <Text style={{color: 'black'}}>Recuperação t2</Text>
+        </TouchableOpacity>
+
+    </Container>
   );
 }
 
