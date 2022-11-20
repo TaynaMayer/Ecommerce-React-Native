8c4f989915dec8c976886b2133e100e6b9556976
Tayná Mayer <112437100+TaynaMayer@users.noreply.github.com>
2022-11-20 17:22:00 -0300

    Tayna (#8)
    * acrescentando a navegação para a t2
    
    * styled components global para input e button

--- Trabalho/Ecommerce/src/screens/Recuperação/RecuperaçãoT2/recuperacaoT2.js
+++ Trabalho/Ecommerce/src/screens/Recuperação/RecuperaçãoT2/recuperacaoT2.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, } from 'react-native';
+import { Container, InputForm, Button} from '../../../components/Global';
 
 // import { Container } from './styles';
 
 const RecuperaçãoT2 = () => {
   return (
-    <Text>
-        RecuperaçãoT2
-    </Text>
+
+    <Container>
+      <Text>MULTI TECH</Text>
+      <InputForm placeholder= "Senha" placeholderTextColor= "#616161" 
+      style= {{textAlign: "center"}} />    
+      <Button>Atualizar senha</Button> 
+      
+    </Container>
   );
 }
 
