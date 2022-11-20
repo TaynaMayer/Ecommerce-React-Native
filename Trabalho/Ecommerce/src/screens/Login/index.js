d10c234599a2463329ca49e19a5c603aa5eb3be1
Gabriel Martins <gabriel-pms@hotmail.com>
2022-11-20 15:22:31 -0300

    consumindo api

--- Trabalho/Ecommerce/src/screens/Login/index.js
+++ Trabalho/Ecommerce/src/screens/Login/index.js
@@ -1,14 +1,15 @@
 import { Text, View, TextInput, TouchableOpacity } from "react-native";
 import { Gradient } from "../../components/index";
+import { Container } from "../../components/Global";
 
 export default function Login() {
 
     return (
-        <View>
+        <Container>
             <Text>MultiTech</Text>
             <TouchableOpacity>
                 <Text>Entrar</Text>
             </TouchableOpacity>
-        </View>
+        </Container>
     )
 }
---
+++ Trabalho/Ecommerce/src/service/api.js
@@ -0,0 +1,10 @@
+import axios from "axios"
+
+
+export const Api = axios.create({
+    baseURL: "https://reactnative.herokuapp.com/reactnative",
+    auth: {
+        username: "reactnative",
+        password: "2022"
+    }
+})
