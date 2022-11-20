5204cd7061d7b3a7e47eb2447c412bee09c2ffc2
Tayná Mayer <112437100+TaynaMayer@users.noreply.github.com>
2022-11-20 17:21:25 -0300

    styled components global para input e button

--- Trabalho/Ecommerce/src/components/Global.js
+++ Trabalho/Ecommerce/src/components/Global.js
@@ -21,3 +21,25 @@ export const Title = styled.Text`
 
     color: #690A0A;
 `
+
+export const InputForm = styled.TextInput`
+
+    background: #F5F5F5;
+    border: 2px solid #FAFAFA;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 5px;  
+    width: 100% ;
+    margin-bottom: 23px;
+
+`;
+
+export const Button = styled.TouchableOpacity`
+
+    background: #690A0A;
+    border-radius: 5px;
+    padding: 9px 80px;    
+    color: #fff;
+    font-family: 'DM Sans';
+    font-size: 12px;
+    
+`;
