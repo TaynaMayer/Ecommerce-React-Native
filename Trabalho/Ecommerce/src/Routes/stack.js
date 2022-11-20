5cb58c2f741184b980bf88d5210cd39d8a8b7aa7
Gabriel Martins <gabriel-pms@hotmail.com>
2022-11-20 14:55:18 -0300

    estruturando pastas e criando paginas

--- Trabalho/Ecommerce/src/Routes/stack.js
+++ Trabalho/Ecommerce/src/Routes/stack.js
@@ -1,6 +1,9 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from "react";
+import Cadastro from '../screens/Cadastro/cadastro';
 import Login from "../screens/Login";
+import RecuperaçãoT1 from '../screens/Recuperação/RecuperaçãoT1/recuperacaoT1';
+import RecuperaçãoT2 from '../screens/Recuperação/RecuperaçãoT2/recuperacaoT2';
 
 const { Screen, Navigator } = createNativeStackNavigator();
 
@@ -9,10 +12,22 @@ export function StackRoutes() {
         <Navigator
             screenOptions={{
                 headerShown: false,
-            }}>
+            }} initialRouteName="Login">
             <Screen
                 name="Login"
                 component={Login}
+            />
+            <Screen
+                name="Cadastro"
+                component={Cadastro}
+            />
+            <Screen
+                name="RecuperaçãoT1"
+                component={RecuperaçãoT1}
+            />
+            <Screen
+                name="RecuperaçãoT2"
+                component={RecuperaçãoT2}
             />
         </Navigator>
     )
--- Trabalho/Ecommerce/src/screens/Login/index.js
+++ Trabalho/Ecommerce/src/screens/Login/index.js
@@ -5,7 +5,6 @@ export default function Login() {
 
     return (
         <View>
-            <Gradient />
             <Text>MultiTech</Text>
             <TouchableOpacity>
                 <Text>Entrar</Text>
