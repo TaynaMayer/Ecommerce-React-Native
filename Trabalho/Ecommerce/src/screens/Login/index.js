2851eeaa5935613a17a244f94c20da6eca7aa2bd
Gabriel Martins <gabriel-pms@hotmail.com>
2022-11-20 15:44:12 -0300

    botão para outras telas

--- Trabalho/Ecommerce/src/screens/Login/index.js
+++ Trabalho/Ecommerce/src/screens/Login/index.js
@@ -1,15 +1,25 @@
 import { Text, View, TextInput, TouchableOpacity } from "react-native";
 import { Gradient } from "../../components/index";
 import { Container } from "../../components/Global";
+import { Header } from "../../components/header/header";
 
 export default function Login() {
 
     return (
         <Container>
-            <Text>MultiTech</Text>
-            <TouchableOpacity>
-                <Text>Entrar</Text>
-            </TouchableOpacity>
+            
+            <Header name="Entrar">
+                 
+            </Header>
+
+            <TouchableOpacity onPress={() => 
+            navigation.navigate("RecuperaçãoT1")
+        }>
+            Esqueci Minha senha
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate("Cadastro")}>
+            Cadastro
+        </TouchableOpacity>
         </Container>
     )
 }
