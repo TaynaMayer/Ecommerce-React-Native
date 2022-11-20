f1bd6912ea8e7baa63b96aeedfa66a1ae904148a
Gabriel Martins <gabriel-pms@hotmail.com>
2022-11-20 14:58:05 -0300

    estruturando pastas e criando paginas (#3)

---
+++ Trabalho/Ecommerce/src/screens/Recuperação/RecuperaçãoT1/recuperacaoT1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// import { Container } from './styles';
+
+const RecuperaçãoT1 = () => {
+  return (
+    <Text>RecuperaçãoT1</Text>
+  );
+}
+
+export default RecuperaçãoT1;
---
+++ Trabalho/Ecommerce/src/screens/Recuperação/RecuperaçãoT2/recuperacaoT2.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// import { Container } from './styles';
+
+const RecuperaçãoT2 = () => {
+  return (
+    <Text>
+        RecuperaçãoT2
+    </Text>
+  );
+}
+
+export default RecuperaçãoT2;
