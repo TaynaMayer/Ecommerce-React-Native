7c0b9b523b21a6b6d863f0797eaebe973b51d73c
Gabriel Martins <gabriel-pms@hotmail.com>
2022-11-20 15:24:35 -0300

    Biti (#4)
    * estruturando pastas e criando paginas
    
    * consumindo api

---
+++ Trabalho/Ecommerce/src/components/Global.js
@@ -0,0 +1,23 @@
+import styled from "styled-components/native"
+
+export const Container = styled.View`
+
+    flex: 1;
+    width: 100%;
+    padding: 40px 45px;
+    align-items: center;
+
+
+`
+
+export const Title = styled.Text`
+
+    font-family: 'DM Sans';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 30px;
+    line-height: 39px;
+    text-align: center;
+
+    color: #690A0A;
+`
