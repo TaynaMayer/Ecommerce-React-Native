15a986f30a474cfc34f2631d8f27d8b8d6806ffd
Gabriel Martins <gabriel-pms@hotmail.com>
2022-11-20 15:47:11 -0300

    Biti (#5)
    * estruturando pastas e criando paginas
    
    * consumindo api
    
    * botão para outras telas

---
+++ Trabalho/Ecommerce/src/components/header/header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+
+// import { Container } from './styles';
+
+const Header = ({ props }) => {
+    const navigation = useNavigation();
+
+  return (
+    <View>
+        <Text></Text>
+
+
+        
+    </View>
+  );
+}
+
+export default Header;
