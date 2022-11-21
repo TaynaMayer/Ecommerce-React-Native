60c78641d27c3ca5f01b9e2b8ede3487541f5e03
Gabriel Martins <gabriel-pms@hotmail.com>
2022-11-20 23:58:42 -0300

    cadastro func

--- Trabalho/Ecommerce/src/screens/Cadastro/cadastro.js
+++ Trabalho/Ecommerce/src/screens/Cadastro/cadastro.js
@@ -1,9 +1,10 @@
-import { Text, Image, } from "react-native";
+import { Text, Image, Platform, TouchableOpacity } from "react-native";
 import { Container, InputForm, Button } from "../../components/Global";
 import Header from '../../components/header/header';
 import { Api } from "../../service/api"
 import { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
+import * as ImagePicker from "expo-image-picker";
 
 export default function Cadastro() {
 
@@ -12,17 +13,41 @@ export default function Cadastro() {
     const [ativo, setAtivo] = useState(true)
     const [cpf, setCpf] = useState("")
     const [nascimento, setNascimento] = useState("")
-    const [foto, setFoto] = useState("")
     const [login, setLogin] = useState("")
     const [nome, setNome] = useState("")
     const [senha, setSenha] = useState("")
+    const [image, setImage] = useState("")
+
+    async () => {
+        if(Platform.OS !== "web") {
+            const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+            if(status != 'granted') {
+                alert("Sem permissão")
+            }
+        }
+    }
+
+    const PickImage = async() => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1
+        })
+        console.log(result)
+        if(!result.canceled) {
+            setImage(result.assets.find((foto) => {
+                return foto.uri.substring(0, 100)
+            }))
+        }
+    }
 
     const addPost = () => {
         const data = {
             ativo: ativo,
             cpf: cpf,
             dtNascimento: nascimento,
-            foto: foto,
+            foto: image,
             login: login,
             nome: nome,
             senha: senha
@@ -30,7 +55,9 @@ export default function Cadastro() {
           
         Api.post("/usuario", data)
            .then((res) => {
-                navigation.goBack();
+            console.log(res.data)
+           }).catch((err) => {
+            console.log(err)
            });
     };
 
@@ -48,6 +75,12 @@ export default function Cadastro() {
             <InputForm value={cpf} onChangeText={(data) => setCpf(data) } placeholder= "CPF" placeholderTextColor= "#616161" />
             <InputForm value={senha} onChangeText={(data) => setSenha(data) } placeholder= "Senha" placeholderTextColor= "#616161" />
             <InputForm value={nascimento} onChangeText={(data) => setNascimento(data) } placeholder= "Data de nascimento" placeholderTextColor= "#616161" />
+            <TouchableOpacity onPress={PickImage}>
+                <Text>Clique Aqui</Text>
+            </TouchableOpacity>
+
+            {image && <Image source={{uri:image}} style={{width: 200, height: 200}}/>}
+            
             <Button onPress={() => addPost()}>
              <Text style= {{color: "white"}}>Cadastrar</Text>
              </Button> 
