6d8cb0f340a00704ee9681187dac667e9dcb8db6
Gabriel Martins <gabriel-pms@hotmail.com>
2022-11-21 20:50:22 -0300

    rotas

--- Trabalho/Ecommerce/src/Routes/stack.js
+++ Trabalho/Ecommerce/src/Routes/stack.js
@@ -1,9 +1,14 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from "react";
 import Cadastro from '../screens/Cadastro/cadastro';
+import Carrinho from '../screens/Carrinho/carrinho';
+import Categoria from '../screens/Categoria/Categoria';
+import DescricaoProduto from '../screens/DescricaoProduto/Produto';
+import Home from '../screens/Home/Home';
 import Login from "../screens/Login";
 import RecuperaçãoT1 from '../screens/Recuperação/RecuperaçãoT1/recuperacaoT1';
 import RecuperaçãoT2 from '../screens/Recuperação/RecuperaçãoT2/recuperacaoT2';
+
 
 const { Screen, Navigator } = createNativeStackNavigator();
 
@@ -29,6 +34,23 @@ export function StackRoutes() {
                 name="RecuperaçãoT2"
                 component={RecuperaçãoT2}
             />
+            <Screen
+                name="Carrinho"
+                component={Carrinho}
+            />
+            <Screen
+                name="Home"
+                component={Home}
+            />
+            <Screen
+                name="Categoria"
+                component={Categoria}
+            />
+            <Screen
+                name="DescricaoProduto"
+                component={DescricaoProduto}
+            />
+            
         </Navigator>
     )
 
