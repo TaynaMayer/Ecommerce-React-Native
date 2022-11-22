1c419b6da3cc1c6326420d3601563fc77e5e8588
Gabriel <gabriel.corretortere@gmail.com>
2022-11-22 13:06:46 -0300

    Subindo carrinho e context

--- Trabalho/Ecommerce/App.js
+++ Trabalho/Ecommerce/App.js
@@ -1,11 +1,15 @@
 import {Routes} from "./src/Routes";
 import { IdProvider } from "./src/context/IdContext";
+import {CartProvider} from"./src/context/Cart";
 
 export default function App() {
   return (
-
+    
     <IdProvider>
+     <CartProvider>
       <Routes />
+    </CartProvider>
+
     </IdProvider>
   );
 }
---
+++ Trabalho/Ecommerce/src/context/Cart.js
@@ -0,0 +1,58 @@
+import { createContext, useState } from "react";
+
+export const CartContext = createContext({});
+
+export const CartProvider = ({ children }) => {
+    const [produtos, setProdutos] = useState([])
+    function adcionarItemAoCarrinho(id, foto, nome, valor, descricao) {
+
+        const item = [...produtos]
+        const itemFiltrado = item.find((prod) => prod.id === id)
+
+        if (!itemFiltrado) {
+            item.push({
+                id: id,
+                foto: foto,
+                nome: nome,
+                valor: valor,
+                descricao: descricao,
+                quantidade: 1
+            })
+
+        } else {
+            item.quantidade = item.quantidade + 1
+        }
+        setProdutos(item)
+    }
+
+    function removerItem(id) {
+        const item = [...produtos]
+        const itemFiltrado = item.find((prod) => prod.id === id)
+        if (itemFiltrado && itemFiltrado.quantidade > 1) {
+            itemFiltrado.quantidade = itemFiltrado.quantidade - 1;
+            setProdutos(item);
+
+        }
+        else {
+            const arrayFiltered = item.filter((product) => product.id !== id);
+            setProdutos(arrayFiltered);
+
+        }
+    }
+    function deleteItem (id){
+
+        const item = [...produtos]
+        const arrayFiltered = item.filter((product) => product.id !== id);
+            setProdutos(arrayFiltered);
+    }
+
+    function limparCarrinho(){
+        setProdutos([]);
+    }
+    return (
+        <CartContext.Provider value={{ produtos, adcionarItemAoCarrinho, removerItem,deleteItem,limparCarrinho }}>
+            {children}
+        </CartContext.Provider>
+    );
+
+}
