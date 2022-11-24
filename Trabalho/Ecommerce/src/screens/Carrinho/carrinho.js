dbcff6c45e60525fee77ca848e291903817cc918
Gabriel Martins <gabriel-pms@hotmail.com>
2022-11-23 22:51:59 -0300

    Biti (#37)
    * estruturando pastas e criando paginas
    
    * consumindo api
    
    * botão para outras telas
    
    * corrigindo erro de navegação
    
    * header component e login
    
    * statusbar e goback
    
    * cadastro func
    
    * terminando post e adcionando safeAreaView
    
    * corrigindo erro
    
    * estilizando
    
    * corrigindo areaview
    
    * função esqueci minha senha
    
    * rotas
    
    * headerHOme
    
    * mexendo com pagina produto e headerProduto
    
    * estilizando header, e fazendo produtos
    
    * pagina produto
    
    * route params descricao
    
    * login
    
    * carrinho

--- Trabalho/Ecommerce/src/screens/Carrinho/carrinho.js
+++ Trabalho/Ecommerce/src/screens/Carrinho/carrinho.js
@@ -2,6 +2,7 @@
 import React, { useContext } from 'react';
 import { Text, View } from 'react-native';
 import { CartContext } from '../../context/Cart';
+import Header from "../../components/header/header"
 
 // import { Container } from './styles';
 
@@ -10,9 +11,14 @@ const Carrinho = () => {
 
   return(
     <View>
+      
       {produtos.length===0&&(
         <Text>Sem produtos</Text>
       )}
+
+      {produtos?.map((produto) => {
+       return <Text key={produto.id}>{produto.nome}</Text>
+      })}
       </View>
 
   );
